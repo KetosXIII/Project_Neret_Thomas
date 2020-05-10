5615991778780623547335990cca8806e9175c22
KetosXIII <59283368+KetosXIII@users.noreply.github.com>
2020-05-10 14:09:41 +0200

    Api commit

---
+++ app/src/main/java/com/example/project_neret_thomas/PokeApi.java
@@ -0,0 +1,12 @@
+package com.example.project_neret_thomas;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeApi {
+    @GET("/api/v2/pokemon")
+Call<RestPokemonResponse> getPokemonResponse();
+    @GET("/api/v2/ability")
+    Call<RestPokemonResponse> getAbility();
+}
